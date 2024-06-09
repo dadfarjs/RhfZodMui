0a8fa7b959ce2d1f06f520f40ec83e2d89f0fbde
golismero96 <golismero96@gmail.com>
2024-06-09 17:54:58 +0330

    add useForm

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useForm } from "react-hook-form";
 
 const Users = () => {
-  return <div>Users</div>;
+  const { register, formState } = useForm<{ email: string }>();
+  return (
+    <>
+      <input
+        {...register("email", {
+          required: {
+            value: true,
+            message: "The email is required",
+          },
+          maxLength: { value: 10, message: "Too many characters" },
+        })}
+        placeholder="Emil"
+      />
+      <p>{}</p>
+    </>
+  );
 };
 
 export { Users };
