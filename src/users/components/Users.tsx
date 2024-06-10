27c87ae80d9211fa2182268c5be40ebc5196c5b8
golismero96 <golismero96@gmail.com>
2024-06-10 13:51:24 +0330

    start form mui

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,21 +1,19 @@
 import { useForm } from "react-hook-form";
+import { Stack, TextField } from "@mui/material";
+import { SchemaType, schema } from "../types/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const Users = () => {
-  const { register, formState } = useForm<{ email: string }>();
+  const { register } = useForm<SchemaType>({
+    mode: "all",
+    resolver: zodResolver(schema),
+  });
+
   return (
-    <>
-      <input
-        {...register("email", {
-          required: {
-            value: true,
-            message: "The email is required",
-          },
-          maxLength: { value: 10, message: "Too many characters" },
-        })}
-        placeholder="Emil"
-      />
-      <p>{}</p>
-    </>
+    <Stack>
+      <TextField label="name" {...register("name")} />
+      <TextField label="email" {...register("email")} />
+    </Stack>
   );
 };
 
