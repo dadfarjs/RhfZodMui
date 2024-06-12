c3f432c63c329d337bee1d363b99d1eb886d671e
golismero96 <golismero96@gmail.com>
2024-06-12 18:22:22 +0330

    Time 1:07:00

--- src/components/RHFAutocomplete.tsx
+++ src/components/RHFAutocomplete.tsx
@@ -1,23 +1,62 @@
 import { Controller, useFormContext, FieldValues, Path } from "react-hook-form";
-import { Autocomplete, TextField } from "@mui/material";
+import { Autocomplete, Box, Checkbox, TextField } from "@mui/material";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+
+import { Option } from "../types/option";
 
 type Props<T extends FieldValues> = {
   name: Path<T>;
+  options: Option[];
+  label: string;
 };
 
-export const RHFAutocomplete = <T extends FieldValues>({ name }: Props<T>) => {
+export const RHFAutocomplete = <T extends FieldValues>({
+  name,
+  options,
+  label,
+}: Props<T>) => {
   const { control } = useFormContext();
   return (
     <Controller
       control={control}
       name={name}
-      render={(params) => (
+      render={({ field: { value, onChange, ref }, fieldState: { error } }) => (
         <Autocomplete
-          options={[
-            { id: "1", label: "Texas" },
-            { id: "2", label: "LA" },
-          ]}
-          renderInput={(params) => <TextField {...params} label="States" />}
+          options={options}
+          value={value.map((id: string) =>
+            options.find((item) => item.id === id)
+          )}
+          getOptionLabel={(option) =>
+            options.find((item) => item.id === option.id)?.label ?? ""
+          }
+          isOptionEqualToValue={(options, newValue) =>
+            options.id === newValue.id
+          }
+          onChange={(_, newValue) => {
+            onChange(newValue.map((item) => item.id));
+          }}
+          disableCloseOnSelect
+          multiple
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label={label}
+              fullWidth
+              inputRef={ref}
+              error={!!error}
+              helperText={error?.message}
+            />
+          )}
+          renderOption={(props, option, { selected }) => (
+            <Box {...props}>
+              <Checkbox
+                icon={<CheckBoxOutlineBlankIcon />}
+                checkedIcon={<CheckBoxIcon />}
+                checked={selected}
+              />
+            </Box>
+          )}
         />
       )}
     />
